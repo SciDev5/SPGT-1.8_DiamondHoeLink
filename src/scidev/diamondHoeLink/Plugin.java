bdd0b3695007045d562a5a37fa75426947b8cda2
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2020-06-25 08:46:56 -0400

    Patched error on re-enableing plugin.
    Removed problem where a BukkitTask was being executed twice.

--- src/scidev/diamondHoeLink/Plugin.java
+++ src/scidev/diamondHoeLink/Plugin.java
@@ -1,23 +1,24 @@
 package scidev.diamondHoeLink;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Plugin extends JavaPlugin {
 	
 	private boolean running = false;
 	
-	private TestPlayersTask recurringTask;
+	//private TestPlayersTask recurringTask;
+	private BukkitTask recurringTask;
 	
 	@Override
 	public void onEnable() {
 		getServer().getPluginCommand("startscenario_diahoelink").setExecutor(new StartCommand(this));
-		recurringTask = new TestPlayersTask(this);
 	}
 	
 	public void setRunning(boolean running) {
 		this.running = running;
 		if (this.running) {
-			recurringTask.runTaskTimer(this, 20*60, 20*60);
+			recurringTask = (new TestPlayersTask(this)).runTaskTimer(this, 20*60, 20*60);
 		} else {
 			recurringTask.cancel();
 		}
